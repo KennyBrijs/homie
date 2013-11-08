1f27a5fd9de2346498082d380f8fb88e6e52359b
kennybrijs <kennybrijs@gmail.com>
2013-11-08 16:35:51 +0100

    oaije

--- client/js/pages/profile.js
+++ client/js/pages/profile.js
@@ -3,5 +3,6 @@ $(function(){
 
     getFacebookProfile(logged_in_homie.username, function (data, textStatus, jqXHR) {
         $(".profile #homie-pic").attr("src", data.picture.data.url);
+        $(".profile #homie-name").attr("src", data.name);
     });
 });
