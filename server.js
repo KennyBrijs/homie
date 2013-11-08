4878f4cdc92df3e62dfb0951cf9b40b3223ac157
kennybrijs <kennybrijs@gmail.com>
2013-11-08 15:13:18 +0100

    Uncomment facebook auth

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ process.on('uncaughtException', function (exception) {
 
 // Ride API
 app.post("/rides/addriderequest", rides.addRideRequest);
-app.post("rides/getriderequests", rides.getRideRequests);
+app.post("/rides/getriderequests", rides.getRideRequests);
 app.post("/rides/acceptriderequest", rides.acceptRideRequest);
 app.post("/rides/getmatchedrequest", rides.getMatchedRequests);
 app.post("/rides/getmatchedrides", rides.getMatchedRides);
@@ -134,19 +134,19 @@ app.get('/login', function(req, res){
 //   request.  The first step in Facebook authentication will involve
 //   redirecting the user to facebook.com.  After authorization, Facebook will
 //   redirect the user back to this application at /auth/facebook/callback
-/*app.get('/auth/facebook',
+app.get('/auth/facebook',
   passport.authenticate('facebook'),
   function(req, res){
     // The request will be redirected to Facebook for authentication, so this
     // function will not be called.
-  });*/
+  });
 
 // GET /auth/facebook/callback
 //   Use passport.authenticate() as route middleware to authenticate the
 //   request.  If authentication fails, the user will be redirected back to the
 //   login page.  Otherwise, the primary route function function will be called,
 //   which, in this example, will redirect the user to the home page.
-/*app.get('/auth/facebook/callback', 
+app.get('/auth/facebook/callback', 
   passport.authenticate('facebook', { failureRedirect: '/login' }),
   function(req, res) {
     res.redirect('/');
@@ -155,7 +155,7 @@ app.get('/login', function(req, res){
 app.get('/logout', function(req, res){
   req.logout();
   res.redirect('/');
-});*/
+});
 
 
 app.use(express.static(__dirname + '/client'));
